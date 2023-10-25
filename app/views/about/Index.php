7b8335b194a53ecd4a4360cf6d0bad4e53d8b445
adanahmaderlangga <adan.213040101@mail.unpas.ac.id>
2023-10-25 13:31:46 +0700

    first commit

---
+++ app/views/about/Index.php
@@ -0,0 +1,6 @@
+<div class="container">
+    
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/Adan.JPG" alt="Adan Ahmad Erlangga" width="200" class="rounded-circle shadow">
+    <p>Halo! Perkenalkan nama saya <?= $data['nama']; ?>, umur saya adalah <?= $data['umur']; ?> tahun, dan saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
